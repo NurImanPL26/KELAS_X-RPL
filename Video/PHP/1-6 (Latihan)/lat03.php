fc2b69d2841a75e12fab018c29fc7e4dbebafd9a
ItsYourVertigo <nurimanpl37@gmail.com>
2025-10-01 18:26:41 +0700

    Add files via upload

---
+++ Video/PHP/1-6 (Latihan)/lat03.php
@@ -0,0 +1,21 @@
+<?php
+function belajar(){
+    echo "saya belajar PHP";
+}
+
+function luasPersegi($p = 5, $l = 3){
+    $luas = $p * $l;
+    echo $luas;
+}
+
+function luas($p = 5, $l = 3){
+    $luas = $p * $l;
+    return $luas;
+}
+
+function output(){
+    return "belajar function";
+}
+
+echo '<h1>'.output().'</h1>';
+?>
