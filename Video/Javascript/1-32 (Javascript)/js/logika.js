90d77b03c020ade59132fd99b737dfc64653bf7e
ItsYourVertigo <nurimanpl37@gmail.com>
2025-11-28 21:40:25 +0700

    Javascript eps 1-40

---
+++ Video/Javascript/1-32 (Javascript)/js/logika.js
@@ -0,0 +1,34 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+// OR bernilai true jika salah sat true
+console.log(d || b || a);
+
+// AND bernilai false jika salah sat false
+console.log(a && b && c);
+
+let x = 8;
+let y = 7;
+
+// Lebih besar
+console.log(x > y);
+
+// Lebih kecil
+console.log(x < y);
+
+// Sama dengan
+console.log(x == y);
+
+// Lebih besar sama dengan
+console.log(x >= y);
+
+// Lebih kecil sama dengan
+console.log(x <= y);
+
+let lima = "5";
+let limo = 5;
+
+// Identik - sama tipe data dan isi
+console.log(lima === limo);
