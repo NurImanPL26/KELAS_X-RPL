b9254f0d9b20d8b23715f12cbab1101798c84669
ItsYourVertigo <nurimanpl37@gmail.com>
2026-01-12 09:40:48 +0700

    Add files via upload

---
+++ Tugas Pak Tri/Kelas X RPL/src/XRPL.java
@@ -0,0 +1,34 @@
+
+
+public class XRPL {
+    
+    public static void main(String[] args) {
+        int angka = 7;
+            System.out.println(angka);
+            
+            double nilai;
+               nilai = 87.5;
+            System.out.println("Nilai anda adalah : " +nilai);
+            
+            float berat=(float) 57.6;
+            System.out.println("Berat badan anda adalah : "  +berat);
+            
+            long populasi=(long) 903.546;
+            System.out.println(populasi);
+            
+            short umur = (short) 19;
+            System.out.println(umur);
+            
+            byte level = (byte) 7;
+            System.out.println(level);
+            
+            char huruf;
+        huruf = (char) 'a';
+            System.out.println(huruf);
+            
+            boolean lulus = true;
+            System.out.println(lulus);
+            
+    }
+    
+}
